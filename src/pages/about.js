7df7abb41eddf73f830050b8ba57e682fc5cb046
Kate Mills <kate.mills1124@gmail.com>
2020-09-08 19:10:42 -0700

    About page

--- src/pages/about.js
+++ src/pages/about.js
@@ -12,17 +12,15 @@ const About = ({data}) => {
     <PageModel title="About"
     description="Each Michele Corley Clinical Skincare product is crafted with care and consideration to the health and well being of the skin. The ingredients I choose must come from the most natural sources available, and their effectiveness proven.">
       <AboutWrapper>
-        <div className="flexItem col" style={{margin: "0 auto" }}>
-          <div className="innerFlexContainer">
-            <h1>My Philosophy</h1>
-            <div className="innerFlexItem">
-              <p className="text">
+        <div className="col-1" style={{margin: "0 auto"}}>
+          <h1>My Philosophy</h1>
+          <p className="text">
                 is simple, to provide healthy, effective skincare with exemplary
                 customer service. I believe in treating my clients' success as
                 important as my own, and value a true partnership with everyone
                 I have the pleasure to work with.
-              </p>
-              <p className="text">
+          </p>
+          <p className="text">
                 My journey in creating Michele Corley Clinical Skincare began in
                 2002 when I wanted to fulfill what I saw as a need in the
                 professional skincare industry to offer not only great products,
@@ -32,8 +30,8 @@ const About = ({data}) => {
                 huge minimums, and fighting for good customer service. I wanted
                 to change all that, by offering completely affordable premium
                 professional skincare with accessible customer service.
-              </p>
-              <p className="text">
+          </p>
+          <p className="text">
                 Each Michele Corley Clinical Skincare product is crafted with
                 care and consideration to the health and well being of the skin.
                 The ingredients I choose must come from the most natural sources
@@ -41,34 +39,27 @@ const About = ({data}) => {
                 that you and your clients will love the results you'll see and
                 feel from using Michele Corley Clinical Skincare, and enjoy the
                 excellence in care you receive.
-              </p>
-              <p className="text">Many thanks.</p>
-              <h2 className="italic"> Michele</h2>
-            </div>
-          </div>
+          </p>
+          <p className="text">Many thanks.</p>
+          <h2 className="italic poppy">Michele</h2>
         </div>
-
-        <div className="col" style={{margin: "0 auto" }}>
-          <div className="innerFlexContainer">
-            <h2 >Meet Michele Corley</h2>
-            <Image fluid={data.allFile.nodes[0].childImageSharp.fluid} alt="Portrait of Michele Corley in a red floral blouse."/>
-            <div className="">
-              <p className="text tiny" style={{ margin: "0 auto", fontSize: "12px", lineHeight: "27px", paddingBlockStart: "10px", paddingBlockEnd: "10px", paddingInlineStart: "15px", paddingInlineEnd: "unset"  }}>
-                Michele is a licensed esthetician and creative visionary behind
-                every Michele Corley Clinical Skincare product. She is deeply
-                committed to educating herself first, so she can then be a
-                resource to support her clients' success. Her passion and drive
-                to create the best possible experience for her clients shines
-                through each Michele Corley Clinical Skincare Product and every
-                client interaction.
-              </p>
-              <p className="text tiny" style={{ margin: "0 auto", fontSize: "12px", lineHeight: "27px", paddingBlockStart: "10px", paddingBlockEnd: "10px", paddingInlineStart: "15px", paddingInlineEnd: "unset"  }}>
-                Michele studied with one of the foremost cosmetic chemistry
-                authorities in the professional skincare industry, and has
-                advanced knowledge of skin aging, acne conditions and rosecea.
-              </p>
-            </div>
-          </div>
+        <div className="col-2" style={{margin: "0 auto"}}>
+          <h2 className="poppy">Meet<br/>Michele Corley</h2>
+          <Image fluid={data.allFile.nodes[0].childImageSharp.fluid} alt="Portrait of Michele Corley in a red floral blouse."/>
+          <p className="tiny">
+              Michele is a licensed esthetician and creative visionary behind
+              every Michele Corley Clinical Skincare product. She is deeply
+              committed to educating herself first, so she can then be a
+              resource to support her clients' success. Her passion and drive
+              to create the best possible experience for her clients shines
+              through each Michele Corley Clinical Skincare Product and every
+              client interaction.
+          </p>
+          <p className="tiny">
+              Michele studied with one of the foremost cosmetic chemistry
+              authorities in the professional skincare industry, and has
+              advanced knowledge of skin aging, acne conditions and rosecea.
+          </p>
         </div>
       </AboutWrapper>
     </PageModel>
@@ -76,64 +67,77 @@ const About = ({data}) => {
 }
 const AboutWrapper = styled.section`
   &{
-    display:flex;
-    flex-wrap:nowrap;
     align-items: center;
-    box-sizing: border-box;
-    display: flex;
-    justify-content: center;
-    margin: 0 auto;
-    line-height: 27px;
+    display:flex;
+    margin: 0 auto !important;
     white-space: initial;
   }
-  & .flexItem { box-sizing: border-box; white-space:initial;  }
-  & h1 {
+  & h1 { 
     text-align: left;
-    margin-top: 20px;
-    margin-bottom: 20px;
+    margin: 20px;
   }
   & h2 { 
    font-style: normal;
-   font-size: 20px;
    text-align: center;
-   margin-top: unset;
    white-space: break-spaces;
    margin-top: 40px;
   }
-  & h2.italic{
+  & .italic{
     font-style: italic;
     text-align: left;
     padding-left: 10%;
     margin-top: unset;
   }
-  & p {padding-left: 5px; white-space: initial;}
-  & p.text{
-    padding-left: 15px;
-    padding-right: 20px;
-    display:block; 
+  & .tiny{
     margin: 0 auto;
+    font-size: 13px !important;
+    line-height: 27px;
+    padding: 1em 2em 0;
+  }
+  & .text{
     text-align: left;
-    font-weight: 300;
     color: var(--mainBlack); 
     font-size: 16px;
-    line-height: 27px;
     white-space: pre-line;
   }
-  & p.text:last-of-type{padding-bottom: 0;}
-  & .flexItem .col{
-    background: red;
+  & .text:last-of-type{
+    padding-bottom: 0;
   }
-  ${screen.minmax.narrow` & { flex-wrap: wrap; } & img {height: auto;}`}
+
+  & .col-1 .text  {
+    padding-left: 40px;
+    padding-right: 40px;
+  }
+
+  & .col-2{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 0 1.5em 0;
+  }
+
+
+  ${screen.minmax.narrow` 
+    & {
+      flex-direction: column;
+    }
+    & .col-1{ width: 100%; }
+    & .col-2{ width: 100%; }
+    & .col-2 .gatsby-image-wrapper{ width: 75%; }
+ `}
   ${screen.minmax.wide`
     & {
       flex-direction: row;
-      flex-wrap: nowrap;
       align-items: flex-start;
       justify-content: space-evenly;
     }
-    & h1{color: var(--mainBlack);}
-    & .col:nth-child(1) { padding-right: 30px;}
-    & .col:nth-child(2) { padding-left: 30px;}
+    & .col-1,
+    & .col-2 { 
+      width: 50%;
+    }
+    & .col-2 .gatsby-image-wrapper{ width: 50%; }
+
   `}
 `
 export const query = graphql`
