c7b7df2f18ff328f2326d61f2d31ffc19804e92a
Kate Mills <kate.mills1124@gmail.com>
2020-09-13 11:34:03 -0700

    continue to move css to root-wrapper

--- root-wrapper.js
+++ root-wrapper.js
@@ -37,6 +37,73 @@ const GlobalStyle = createGlobalStyle`
   box-sizing: border-box;
   font-family: var(--mainFont);
 }
+body {
+    background: var(--mainMcc);
+    background-color: var(--mainMcc);
+    background-image: none;
+    background-size: auto;
+    background-repeat: repeat;
+    color: var(--mainBlack);
+    display: block;
+    font-family:var(--mainFont);
+    font-style: normal;
+    font-size: 16px;
+    font-weight: 300;
+    line-height: var(--bodyLineHeight);
+    overflow: scroll;
+    text-transform: none;
+    text-decoration: none;
+    margin: 0;
+    min-height: 100vh;
+}
+main{
+  background: var(--mainWhite);
+  background-color: var(--mainWhite);
+  border: 0;
+  border-radius: 25px;
+  color: var(--mainBlack);
+  font-family: var(--mainFont);
+  font-size: 16px;
+  font-weight: 300;
+  margin: 0 auto;
+  padding: 0;
+  width: 100%;
+}
+h1, h2, h3, h4, h5, h6 {
+  font-family: var(--mainFont);
+  font-weight: 300;
+  line-height: var(--headingLineHeight);
+  margin: 0.5em 0 0.3em;
+  color: var(--mainBlack);
+}
+h1 {
+  font-size: 43px;
+  font-style: normal;
+  font-weight: 300;
+  letter-spacing: var(--mainSpacing);
+  margin: 20px auto;
+}
+h2, h3{
+  font-size: 30px;
+  font-weight: 300;
+}
+a {
+  color: var(--mainBlack);
+  cursor: pointer;
+  text-decoration: none;
+}
+a:focus{
+  outline: none;
+  background: inherit;
+  text-decoration: none;
+}
+hr {
+  background: rgba(0, 0, 0, 0.1);
+  border: none;
+  height: 1px;
+  margin: 0 auto;
+}
+
 
 
 `
