83bca3e264f68302f129d963f0a4509bf8856997
Kate Mills <kate.mills1124@gmail.com>
2020-09-22 18:06:49 -0700

    single product image template finished

--- src/templates/product-image-template.js
+++ src/templates/product-image-template.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import SEO from '../components/seo'
-import Layout from '../components/layout'
 
 import styled from 'styled-components'
 import Img from 'gatsby-image'
@@ -10,32 +9,29 @@ import { graphql } from 'gatsby'
 import AniLink from "gatsby-plugin-transition-link/AniLink";
 
 
-const ProductTemplate = ({ data: { product } , className}) => {
+const ProductTemplate = props => {
+  const { data: { product }, className } = props
+  console.log(props);
   return (
-    <Layout>
-    <SEO title={`Retail size ${product.name}`} description={product.description.description}/>
+    <>
+    <SEO title={`${product.fluidImg.title}`} description={product.description.description}/>
       <div className={`${className} grid-container`}>
-        <div className="grid-heading">
-          <span className="grid-name txt-center">{product.name}</span>
-          <AniLink fade to="/product-images-and-logos/" className="btn grid-close">x</AniLink>
+        <div className="grid-top w-100">
+          <AniLink fade to="/product-images-and-logos/" className="grid-close">X </AniLink>
         </div>
-        <div className="grid-img">
-            <Img fluid={product.fluidImg.fluid} title={product.fluidImg.title} alt={product.fluidImg.description} />
-        </div>
+        <div className="grid-img"><Img fluid={product.fluidImg.fluid} title={product.fluidImg.title} alt={product.fluidImg.description}/></div>
         <div className="grid-desc">{product.fluidImg.description}</div>
       </div>
-    </Layout>
+    </>
   )
 }
 
 export const query = graphql`
   query GetMccImage($slug: String) {
     product: contentfulMccProduct(slug: { eq: $slug }) {
-      name
       description {
         description
       }
-
       fluidImg:imgRetail {
         id
         title
@@ -44,60 +40,53 @@ export const query = graphql`
           ...GatsbyContentfulFluid
         }
       }
-      award
-      awardImage{
-        fixed(width:100, height: 100){
-          ...GatsbyContentfulFixed
-        }
-      }
     }
   }
 `
 export default styled(ProductTemplate)`
+  background-color: var(--mainBlack);
   border: 4px solid var(--mainBlack);
   box-sizing: border-box;
   color: var(--mainWhite);
   display: grid;
-  margin: 0 auto;
-  text-align: center;
+  height: 100vh;
   grid-template-columns: repeat(1, 1fr);
-  grid-template-rows: 50px;
-  background-color: var(--mainBlack);
+  grid-template-rows: 45px min-content 10%;
+  margin: 0 auto;
+  overflow: hidden;
+  text-align: center;
   width: 100%;
 
-  & .grid-heading{
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
+  & .grid-top{
+    text-align: right;
+    padding-top: 15px;
+    padding-right: 15px;
   }
-  & .grid-name{
-    font-size: 20px;
-    width: 100%;
+  & .w-100{
+    text-align: right;
+    max-height: 45px;
   }
   & .grid-close{
-    font-size: 30px;
-    color: var(--mainWhite);
-    text-align: right;
-    padding: 5px;
     background: var(--mainBlack);
-    border: 0px solid var(--mainBlack);
-    box-shadow: unset;
+    color: var(--mainWhite);
+    font-size: 2.5em;
   }
   & .grid-close:hover{
     color: var(--poppy);
-    background: var(--mainBlack);
-    border: 0px solid var(--mainBlack);
-    box-shadow: unset;
+    cursor: pointer;
   }
   & .grid-img{
-    box-shadow: var(--light-shadow);
+    max-height: 80vh;
   }
-  & img{
-    object-fit: cover !important;
+  & img {
+    object-fit: contain !important;
+    margin-top: 5px;
+    box-shadow: var(--dark-shadow);
+    max-height: 80vh;
   }
-  @media (max-width: 400px){
-  }
-  @media (min-width: 401px) and (max-width: 800px){
+  & .grid-desc{
+    font-size: 1.5em;
+    letter-spacing: var(--mainSpacing);
+    margin-top: 15px;
   }
 `
-
