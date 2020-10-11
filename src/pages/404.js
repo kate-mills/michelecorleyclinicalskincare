558efa7c736721eeaf34ac5a78a145d198ef0093
Kate Mills <kate.mills1124@gmail.com>
2020-10-11 10:04:35 -0700

    finish error page

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,8 +5,6 @@ import PageModel from '../components/PageModel'
 import styled from 'styled-components'
 import AniLink from 'gatsby-plugin-transition-link/AniLink'
 
-import { setFont, setFlexContainer } from '../css/js/helper-styles'
-
 const ErrorPage = () => {
   return (
     <PageModel name="Oops" title="Oops">
@@ -17,18 +15,19 @@ const ErrorPage = () => {
             We couldn't find the page you were looking for. This is either
             because:
           </p>
-          <ul>
+          <ul data-bullet-list>
             <li>
-              There is an error in the URL entered into your web browser. Please
-              check the URL and try again.
+              <p>There is an error in the URL entered into your web browser.</p>
             </li>
-            <li>The page you are looking for has been moved or deleted.</li>
+            <li><p>The page you are looking for has been moved or deleted.</p></li>
           </ul>
-          <p>
+          <p className=" m-top">
+            Please check the URL and try again or click below to return to our home page.
+          </p>
+          <p className="txt-center m-top">
             {' '}
-            You can return to our homepage by clicking{' '}
-            <AniLink fade to="/">
-              here
+            <AniLink className="btn"  fade to="/">
+              Return to our home page
             </AniLink>
             .
           </p>
@@ -38,33 +37,40 @@ const ErrorPage = () => {
   )
 }
 const ErrorWrapper = styled.section`
+  & {
+    font-family: inherit;
+  }
   & div h1 {
     margin-left: 5px;
   }
   & div {
-    ${setFlexContainer({ direction: 'column', y: 'left' })};
     margin: 10px auto;
-    width: 80%;
+    width: 70%;
   }
   & div p {
-    ${setFont({
-      size: '15px',
-      height: '27px',
-      color: '#5c5c5c',
-      weight: '300',
-    })};
+    font-family: inherit;
+    font-size: 15px;
+    font-height: 27px;
+    color: #5c5c5c;
+    font-weight: 300;
   }
-
   & div h1.oops {
-    ${setFont({
-      size: '45px',
-      height: '1.2em',
-      color: '#222222',
-      weight: '300',
-    })};
+    font-size: 45px;
+    font-height: 1.2em;
+    color: #222222;
+    font-weight: 300;
   }
   & div li {
     margin-left: 40px;
   }
+  & p.m-top{
+    margin-top: 20px;
+  }
+  @media (max-width: 749px) {
+    & div{
+      width 100%;
+    }
+  }
+
 `
 export default ErrorPage
