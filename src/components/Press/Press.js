b4ca7ca1d48be1dd63567e7e55b0e75b273953f2
Kate Mills <helloworldloveksm@icloud.com>
2022-05-13 16:14:12 -0700

    seo headers and sr-only class

--- src/components/Press/Press.js
+++ src/components/Press/Press.js
@@ -4,6 +4,7 @@ import styles from './press.module.css'
 import AniLink from 'gatsby-plugin-transition-link/AniLink'
 
 const Press = ({ data: { data } }) => {
+  let readMorePhrase = 'Read more about '.concat(data.summary.split(' was ')[0])
   return (
     <div className={styles.pr__row}>
       <Img
@@ -11,20 +12,23 @@ const Press = ({ data: { data } }) => {
         className={styles.pr__col}
       />
       <div className={`${styles.pr__col} ${styles.pr__flexible__col}`}>
-        <p>{data.summary}</p>
+        <h3>{data.summary}</h3>
         {data.link && (
           <a
+          tabIndex={0}
             href={data.link}
             className={`${styles.read__more} btn btn-white`}
             target="_blank"
             rel="noreferrer"
           >
             Read More
+            <h4 className="sr-only">{readMorePhrase}</h4>
           </a>
         )}
         {data.relativeLink && (
-          <AniLink fade to={data.relativeLink} className="btn">
+          <AniLink fade tabIndex={0} to={data.relativeLink} className="btn">
             Read More
+          <h4 className="sr-only">{readMorePhrase}</h4>
           </AniLink>
         )}
       </div>
--- src/components/Press/PressList.js
+++ src/components/Press/PressList.js
@@ -4,6 +4,12 @@ import Press from './Press'
 const AllPressList = ({ pressList }) => {
   return (
     <section className="press-list">
+      <h2 className="sr-only">
+        Skin Care in the Press Award-winning professional skincare products for
+        dry and oily skin types to sensitive, sun-damaged and acne-prone skin
+        exclusively sold through licensed skincare professionals.
+      </h2>
+
       {pressList.map(data => {
         return <Press key={data.id} data={data} />
       })}
