5f6850fa73f0096db5634c3ea2c82f1d68676d9a
Kate Mills <helloworldloveksm@icloud.com>
2022-08-26 08:59:24 -0700

    add youtube icon

--- src/constants/social.js
+++ src/constants/social.js
@@ -1,11 +1,21 @@
 import React from 'react'
-import { GrFacebook, GrPinterest, GrVimeo, GrInstagram } from 'react-icons/gr'
+import { GrFacebook, GrPinterest, GrVimeo, GrInstagram, GrYoutube   } from 'react-icons/gr'
 
 export default [
   {
     icon: <GrFacebook />,
     label: 'Michele Corley Clinical Skin Care on Facebook',
     href: 'https://www.facebook.com/michelecorleyclinicalskincare/',
+  },
+  {
+    icon: <GrInstagram />,
+    href: 'https://www.instagram.com/michelecorleyskincare/',
+    label: 'Michele Corley Clinical Skin Care on Instagram',
+  },
+  {
+    icon: <GrYoutube />,
+    href: 'https://www.youtube.com/channel/UCjVjZL0axnqV9EFCrCp3yaQ',
+    label: 'Michele Corley Clinical Skin Care on Youtube',
   },
   {
     icon: <GrPinterest />,
@@ -17,9 +27,5 @@ export default [
     href: 'https://vimeo.com/michelecorley',
     label: 'Michele Corley Clinical Skin Care on Vimeo',
   },
-  {
-    icon: <GrInstagram />,
-    href: 'https://www.instagram.com/michelecorleyskincare/',
-    label: 'Michele Corley Clinical Skin Care on Instagram',
-  },
+
 ]
