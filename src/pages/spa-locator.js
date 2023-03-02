2a92fe34709808d7eccbf517ea851fdbeea885e9
Kate Mills <helloworldloveksm@icloud.com>
2023-03-02 12:29:18 -0800

    seo info

--- src/pages/spa-locator.js
+++ src/pages/spa-locator.js
@@ -1,12 +1,13 @@
 import React from 'react'
+import { graphql } from 'gatsby'
 import styled from 'styled-components'
 import Layout from '../components/layout'
-import {SpaLocator} from '../components/SpaLocator'
+import { SpaLocator } from '../components/SpaLocator'
 
-const SpaLocatorPage = () => {
+const SpaLocatorPage = ({data}) => {
   return (
-    <Layout title="Spa Locator">
-      <h1 style={{marginBottom: '0'}}>Spa Locator</h1>
+    <Layout title="Find Our Products Near You" image={data?.file?.publicURL}>
+      <h1 style={{ marginBottom: '0' }}>Spa Locator</h1>
       <SpaLocatorPageWrapper>
         <h2 className={'center'}>
           Find Our Products At These Professional Locations
@@ -23,9 +24,19 @@ const SpaLocatorPageWrapper = styled.section`
     padding: 0 0 0 0;
     > h2 {
       text-align: center;
-      font-size: .8125rem;
+      font-size: 0.8125rem;
       margin: 0;
     }
   }
 `
 export default SpaLocatorPage
+
+export const query = graphql`
+  query {
+    file(
+      relativePath: { eq: "slideshow/cleansers-with-powder-1280x800-blue.jpg" }
+    ) {
+      publicURL
+    }
+  }
+`
