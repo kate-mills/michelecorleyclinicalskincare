76b3dff1ef601c60934628de460947cb22853d49
Kate Mills <helloworldloveksm@icloud.com>
2023-03-02 17:56:26 -0800

    css touch ups

--- src/pages/spa-locator.js
+++ src/pages/spa-locator.js
@@ -7,11 +7,9 @@ import { SpaLocator } from '../components/SpaLocator'
 const SpaLocatorPage = ({data}) => {
   return (
     <Layout title="Find Our Products Near You" image={data?.file?.publicURL}>
-      <h1 style={{ marginBottom: '0' }}>Spa Locator</h1>
       <SpaLocatorPageWrapper>
-        <h2 className={'center'}>
-          Find Our Products At These Professional Locations
-        </h2>
+        <h1 style={{ marginBottom: '0'}}>In Spas Near You</h1>
+        <h2 className={'center'}>Find Our Products At These Professional Locations</h2>
         <SpaLocator />
       </SpaLocatorPageWrapper>
     </Layout>
@@ -22,6 +20,13 @@ const SpaLocatorPageWrapper = styled.section`
     width: 100%;
     margin: 0 0 0 0;
     padding: 0 0 0 0;
+    > h1{
+      font-family: mr-eaves-modern,sans-serif;
+      font-weight: 400;
+      @media(min-width: 500px){
+        font-family: var(--mainFont);
+      }
+    }
     > h2 {
       text-align: center;
       font-size: 0.8125rem;
