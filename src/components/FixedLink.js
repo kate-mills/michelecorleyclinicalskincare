bdccff3d8231c2c108f504d6201b076445357790
Kate Mills <helloworldloveksm@icloud.com>
2024-07-16 14:02:00 -0700

    hoverable

--- src/components/FixedLink.js
+++ src/components/FixedLink.js
@@ -1,74 +1,77 @@
 import React from 'react'
 import styled from 'styled-components'
 import AniLink from 'gatsby-plugin-transition-link/AniLink'
-import {navigate} from 'gatsby'
+import { navigate } from 'gatsby'
 
-
-const FixedLink = ({ className, to="/spa-locator/", text="FIND OUR PRODUCTS NEAR YOU", isPageLink=false }) => {
-
+const FixedLink = ({
+  className,
+  to = '/spa-locator/',
+  text = 'FIND OUR PRODUCTS NEAR YOU',
+  isPageLink = false,
+}) => {
   return (
     <div className={`${className} fixed-link`}>
-    {
-      !isPageLink ?(
-        <AniLink
-        fade
-        to={to}
-        className={`link-content`}
-        tabIndex={0}
-      >
-      {text}
-        </AniLink>): (
-          <span
-          tabIndex="0"
+      {!isPageLink ? (
+        <AniLink fade to={to} className={`link-content`}>
+          {text}
+        </AniLink>
+      ) : (
+        <span className={`link-content`}
+          tabIndex={0}
           role="link"
-          onClick={()=> navigate(to)}
-          onKeyDown={()=> navigate(to)}
-          className={`link-content`}
-          > {text}</span>
-        )
-    }
+          onClick={() => navigate(to)}
+          onKeyDown={() => navigate(to)}
+        >{text}</span>
+      )}
     </div>
   )
 }
 
 export default styled(FixedLink)`
   & {
-    font-family: var(--mainFont);
     background: var(--poppy);
-    position: fixed;
-    right: 20px;
-    bottom: 15px;
-    z-index: 9998;
-    display: table-cell;
-    width: auto;
-    padding: 20px 15px;
-    letter-spacing: normal;
-    word-spacing: 1px;
     border-radius: 5px;
+    bottom: 15px;
     box-shadow: var(--light-shadow);
-    text-align: center;
-    vertical-align: middle;
-    font-size: 13px;
-    font-weight: 500;
+    display: table-cell;
+    height: auto;
     line-height: 1.5;
     opacity: 1;
+    position: fixed;
+    right: 20px;
+    text-align: center;
+    vertical-align: middle;
+    width: auto;
+    z-index: 9998;
     > .link-content {
-      display: table-cell;
-      height: auto;
-      text-align: center;
-      vertical-align: middle;
+      border-radius: 5px;
       color: var(--mainWhite);
-      white-space: wrap;
+      display: block;
+      font-family: var(--mainFont);
+      font-size: 13px;
+      font-weight: 500;
+      height: auto;
+      letter-spacing: normal;
+      padding: 20px 15px;
+      text-align: center;
       text-overflow: ellipsis;
+      vertical-align: middle;
+      white-space: wrap;
+      word-spacing: 1px;
     }
     @media (max-width: 300px) {
+      bottom: 0;
       left: 0;
       right: 0;
-      bottom: 0;
-      max-width: fit-content;
-      max-height: fit-content;
-      padding: 10px;
+      height: fit-content;
+      text-align: center;
       z-index: 9998;
+      > .link-content {
+        padding: 10px;
+      }
+    }
+    &:hover {
+      cursor: pointer;
     }
   }
 `
