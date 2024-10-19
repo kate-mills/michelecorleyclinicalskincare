dbd7ebdd7573d0d0053b00b890bf149ece253573
Kate Mills <kate.mills1124@gmail.com>
2024-10-19 11:35:58 -0700

    improve (CLS) - no slider on mobile

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -6,7 +6,6 @@ export default function Slideshow({ images = [], interval = 7500 }) {
   const [currentSlide, setCurrentSlide] = useState(0)
 
   useEffect(() => {
-
     const loop = setInterval(() => {
       currentSlide === images.length - 1
         ? setCurrentSlide(c => 0)
@@ -17,13 +16,28 @@ export default function Slideshow({ images = [], interval = 7500 }) {
   }, [images, currentSlide, interval])
 
   return (
-    <Img
-      loading={currentSlide < 1 ? 'eager': 'lazy'}
-      fadeIn={currentSlide < 1 ? false: true}
-      //fetchPriority={currentSlide < 2 ? 'high': 'low'}
-      fluid={images[currentSlide].childImageSharp.fluid}
-      alt={`Michele Corley product highlight`}
-      className={currentSlide < 1 ? styles.slideZero: styles.opacity}
-    />
+    <div>
+      <div className={styles.mobileContainer}>
+        <Img
+          className={styles.slideZero}
+          loading="eager"
+          fluid={images[1].childImageSharp.fluid}
+          alt={`Michele Corley product highlight`}
+        />
+      </div>
+
+      <div className={styles.sliderContainer}>
+        <div className={styles.sliderHeight}>
+          <Img
+            loading={currentSlide < 1 ? 'eager' : 'lazy'}
+            fadeIn={currentSlide < 1 ? false : true}
+            //fetchPriority={currentSlide < 2 ? 'high': 'low'}
+            fluid={images[currentSlide].childImageSharp.fluid}
+            alt={`Michele Corley product highlight`}
+            className={currentSlide < 1 ? styles.slideZero : styles.opacity}
+          />
+        </div>
+      </div>
+    </div>
   )
 }
