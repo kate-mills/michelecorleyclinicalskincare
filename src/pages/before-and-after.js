054813ade22b6f9b70ce0a689d0cbc479e833799
Kate Mills <kate.mills1124@gmail.com>
2025-03-08 12:35:19 -0800

    query sorting update

--- src/pages/before-and-after.js
+++ src/pages/before-and-after.js
@@ -3,30 +3,28 @@ import { graphql } from 'gatsby'
 
 import { GlobalLayout, BeforeAndAfters } from '../components'
 
-
 export default function BeforeAndAfterPage({ data }) {
   return (
     <GlobalLayout>
       <h1>Before & After Page</h1>
-      <p style={{paddingBottom: 0}}>
-          These photos are provided by my dear clients utilizing only Michele
-          Corley Clinical Skin Care Products (unless noted). The results were
-          achieved by using proper home care product daily and most clients also
-          received regular facials. No equipment beyond a steamer and magnifying
-          lamp were used. These photos are not retouched and are generally taken
-          with cellular phones.
-        </p>
-        <BeforeAndAfters lst={data.allAirtable.nodes} />
+      <p style={{ paddingBottom: 0 }}>
+        These photos are provided by my dear clients utilizing only Michele
+        Corley Clinical Skin Care Products (unless noted). The results were
+        achieved by using proper home care product daily and most clients also
+        received regular facials. No equipment beyond a steamer and magnifying
+        lamp were used. These photos are not retouched and are generally taken
+        with cellular phones.
+      </p>
+      <BeforeAndAfters lst={data.allAirtable.nodes} />
     </GlobalLayout>
   )
 }
-
 
 export const query = graphql`
   {
     allAirtable(
       filter: { table: { eq: "BeforeAfter" } }
-      sort: { fields: data___name, order: ASC }
+      sort: { data: { name: ASC } }
     ) {
       nodes {
         id
