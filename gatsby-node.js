419212facea563ba5610fd0d9df6de1d14f8502b
Kate Mills <kate.mills1124@gmail.com>
2025-05-07 12:05:57 -0700

    conditional img

--- gatsby-node.js
+++ gatsby-node.js
@@ -108,7 +108,7 @@ exports.createPages = async ({ graphql, actions }) => {
         slug: logo.slug,
         title: `Download Michele Corley ${logo.name}`,
         description: `Download Michele Corley ${logo.name}`,
-        image: logo.images[0].localFile.publicURL,
+        image: logo?.images[0]?.localFile?.publicURL,
       },
     })
   })
