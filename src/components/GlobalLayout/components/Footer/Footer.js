29b54edc1000d3f705158e526186c113e31a8f4e
Kate Mills <kate.mills1124@gmail.com>
2025-06-26 19:31:52 -0700

    infinity svg -- updated 2025 images

--- src/components/GlobalLayout/components/Footer/Footer.js
+++ src/components/GlobalLayout/components/Footer/Footer.js
@@ -1,6 +1,4 @@
 import React from 'react'
-import { useStaticQuery, graphql } from 'gatsby'
-import { GatsbyImage } from 'gatsby-plugin-image'
 import styled from 'styled-components'
 import socialLinks from './social-links'
 import {
@@ -9,30 +7,22 @@ import {
   AddressInfo,
 } from '../../../../constants/contact-info'
 
-const query = graphql`
-  {
-    infinity: file(relativePath: { eq: "infinity.png" }) {
-      childImageSharp {
-        gatsbyImageData
-      }
-    }
-  }
-`
-
 const Footer = ({ className }) => {
-  const {
-    infinity: {
-      childImageSharp: { gatsbyImageData },
-    },
-  } = useStaticQuery(query)
-
   return (
     <footer className={className}>
-      <GatsbyImage
-        className="infinity-img"
-        image={gatsbyImageData}
-        alt="Infinity Symbol"
-      />
+      <svg
+        id="infinity"
+        width="1036"
+        height="35"
+        viewBox="0 0 1036 35"
+        fill="none"
+      >
+        <path
+          d="M0 18H396M640 18H1036M516.498 17.5001C506.5 23 498.5 28.5 478 31C463.5 31.7525 447.509 25.1165 447.5 17.5001C449.5 9.5 459.002 4.00003 474.5 4C489.998 3.99998 503 10 516.498 17.5001ZM519.039 17.4999C529.037 12 537.037 6.5 557.537 4.00002C572.037 3.24745 588.029 9.88347 588.037 17.4999C586.037 25.5 576.535 31 561.037 31C545.539 31 532.537 25 519.039 17.4999Z"
+          stroke="#7A6F6C"
+          strokeWidth="6"
+        />
+      </svg>
       <p className="follow-us-on">
         Follow Us On<span className="sr-only">{` Social Media`}</span>
       </p>
@@ -84,8 +74,10 @@ const FooterWrapper = styled(Footer)`
     font-weight: 400;
     position: sticky;
     top: 100%;
-    div.infinity-img {
-      max-width: 400px;
+    #infinity {
+      min-width: 250px;
+      width: 500px;
+      max-width: 70%;
     }
     p {
       align-items: center;
@@ -128,7 +120,7 @@ const FooterWrapper = styled(Footer)`
         }
       }
     }
-    @media(max-width: 900px){
+    @media (max-width: 900px) {
       padding-block-end: 70px;
     }
   }
