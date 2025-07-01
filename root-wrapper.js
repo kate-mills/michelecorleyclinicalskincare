886ee1f6e3780b1f518d5523acbe2755c1587efa
Kate Mills <kate.mills1124@gmail.com>
2025-07-01 16:12:02 -0700

    variable font

--- root-wrapper.js
+++ root-wrapper.js
@@ -2,10 +2,10 @@ import * as React from 'react'
 
 import './src/css/main.css'
 
-import '@fontsource/montserrat/400.css';
-import '@fontsource/montserrat/600.css';
+//import '@fontsource/montserrat/400.css';
+//import '@fontsource/montserrat/600.css';
 
-//import '@fontsource-variable/montserrat';
+import '@fontsource-variable/montserrat';
 
 export const wrapRootElement = ({element}) => {
   return (
