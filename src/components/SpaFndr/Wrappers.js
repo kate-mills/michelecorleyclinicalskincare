7e31807d5fd4954a1283d30e4edb31ff8428ddce
Kate Mills <kate.mills1124@gmail.com>
2025-06-30 18:57:58 -0700

    minimize css

--- src/components/SpaFndr/Wrappers.js
+++ src/components/SpaFndr/Wrappers.js
@@ -10,18 +10,17 @@ export const StyledSearchResults = styled.section`
       padding-left: ${JUSTIFY_LEFT_PX};
       margin-left: 0;
       h5 {
-        font-style: normal;
         font-size: small;
         line-height: 3;
         margin-inline-start: 0;
         margin-inline-end: 2px;
-        color: #70757a;
       }
     }
   }
 `
 export const StyledSpaLocatorForm = styled.form`
   & {
+    min-height: 50px;
     margin: 0 auto;
     width: 100%;
     background-color: var(--mainWhite);
@@ -34,7 +33,6 @@ export const StyledSpaLocatorForm = styled.form`
       border: 2px solid #ccc;
       border-radius: 4px;
       display: block;
-      font-size: 1rem;
       margin: 20px auto 0;
       padding: 12px 20px 12px 40px;
       transition: width 0.4s ease-in-out;
@@ -63,6 +61,7 @@ export const StyledSpaList = styled.ul`
     white-space: nowrap;
     margin: 0 auto;
     width: 100%;
+
     @media (min-width: 700px) {
       display: grid;
       gap: 2px 2.5633333px;
@@ -70,6 +69,7 @@ export const StyledSpaList = styled.ul`
       width: calc(100% + 15px);
       margin-left: -10px;
     }
+
     > *,
     * {
       white-space: nowrap;
@@ -86,94 +86,89 @@ export const StyledSpaList = styled.ul`
       border: 0.5566666px solid #e9e9e9;
       > .spa-name {
         padding-inline-start: ${JUSTIFY_LEFT_PX};
-        padding-inline-end: 2rem;
         h4 {
-          font-size: .8rem;
-          font-style: normal;
-          line-height: 1;
-          padding: 0;
-          padding-block-end: 8px;
+          font-size: 0.9rem;
+          line-height: normal;
+          padding: 0 0 8px;
           word-break: break-word;
           min-width: fit-content;
           overflow-wrap: normal;
           white-space: pre-wrap;
         }
       }
+
       > address {
+        font-style: normal; /* overwrite default address */
         padding-inline-start: ${JUSTIFY_LEFT_PX};
         margin-inline: 0;
         overflow: hidden;
         text-overflow: ellipsis;
-        line-height: normal;
-        > * {
-          font-style: normal;
-          font-size: 0.8125rem;
-          line-height: 0.9;
+        line-height: 1;
+        * {
+          font-size: 0.8rem;
         }
-      }
-      > address.spa-location {
-        padding-block-end: 0.3rem;
-        div.spa-addr-locality div {
-          display: inline-block;
-          vertical-align: top;
-          margin: 0;
-          margin-block: 5px;
-          span.city-name {
+
+        &.spa-location {
+          padding-block-end: 0.3rem;
+
+          div.spa-addr-locality div {
             display: inline-block;
             vertical-align: top;
-            :not(:first-of-type) {
-              padding-inline-start: 3px;
+            margin: 5px 0;
+
+            span.city-name {
+              display: inline-block;
+              vertical-align: top;
+              :not(:first-of-type) {
+                padding-inline-start: 3px;
+              }
+            }
+            span.highlight {
+              background-color: var(--offWhite);
             }
           }
-          span.highlight {
-            background-color: #e1e7ec96;
-          }
-        }
-        div.zip.wrap-zip {
-          display: contents;
-          > span.zipcode {
-            display: block;
-            width: fit-content;
-          }
-        }
-        .space {
-          display: inline-block;
-          margin: 0;
-          min-width: 3px;
-          padding-inline-end: 3px;
-        }
-      }
-      > address.spa-urls {
-        position: relative;
-        > div {
-          line-height: normal;
-          margin-block: 0.5rem;
-          max-width: fit-content;
-        }
-        div.web {
-          span {
-            background: #fff;
-            position: absolute;
-            left: 4px;
-            padding-inline-end: 4px;
-            svg {
-              color: var(--darkGrey);
-              font-size: 0.8rem;
+
+          div.zip.wrap-zip {
+            display: contents;
+            > span.zipcode {
+              display: block;
+              width: fit-content;
             }
           }
+          .space {
+            display: inline-block;
+            margin: 0;
+            min-width: 3px;
+            padding-inline-end: 3px;
+          }
         }
-        & a {
+
+        &.spa-urls {
           position: relative;
-          margin-block-start: 1rem;
-          text-decoration-line: underline;
-          text-decoration-style: solid;
-          text-decoration-thickness: auto;
-          vertical-align: top;
-          letter-spacing: normal;
-          line-height: 1;
-          :hover {
-            text-decoration-color: var(--poppyDark);
-            color: var(--poppyDark);
+          div {
+            line-height: normal;
+            margin-block: 0.5rem;
+            max-width: fit-content;
+            &.web {
+              span {
+                svg {
+                  position: absolute;
+                  left: 4px;
+                }
+              }
+            }
+            & a {
+              margin-block-start: 1rem;
+              text-decoration-line: underline;
+              text-decoration-style: solid;
+              text-decoration-thickness: auto;
+              vertical-align: top;
+              letter-spacing: normal;
+              :hover {
+                text-decoration-color: var(--poppyDark);
+                color: var(--poppyDark);
+              }
+            }
           }
         }
       }
