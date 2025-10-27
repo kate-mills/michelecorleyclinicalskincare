8574706c14621aa52d6f9127c137aa202740d94b
Kate Mills <kate.mills1124@gmail.com>
2025-10-27 15:16:15 -0700

    zoom

--- src/components/GlobalLayout/components/ProductSearch/ProductSearch.js
+++ src/components/GlobalLayout/components/ProductSearch/ProductSearch.js
@@ -177,6 +177,8 @@ const StyledProductSearch = styled.div`
     @media screen and (max-width: 767px) {
       & form,
       & table {
+        zoom: 0.8 !important;
+
         & .search__input {
         }
       }
